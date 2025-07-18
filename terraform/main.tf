ea1c1deec0238e51dcb836dc7e2104cb21b73844
Ubuntu <ubuntu@ip-172-31-39-56.eu-west-2.compute.internal>
2025-07-18 08:25:47 +0000

    fix ci/cd pipeline issues
    - add terraform versions file
    - fix terraform configuration
    - add go setup for docker builds
    - improve testing with port-forward
    - add better debugging info

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
