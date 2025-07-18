27b672ea774a68baaabeeb73e1ec19bb4467218c
Ubuntu <ubuntu@ip-172-31-39-56.eu-west-2.compute.internal>
2025-07-18 08:37:28 +0000

    switch to kubectl apply approach
    - remove kubernetes provider to avoid timing issues
    - use kubectl apply with manifest files instead
    - deploy ingress controller and apps in proper order
    - keep terraform for cluster management only

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,5 @@
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "kind-${var.cluster_name}"
-}
+# Note: We'll use kubectl apply instead of the kubernetes provider
+# to avoid provider initialization issues
 
 # Create the cluster
 module "cluster" {
@@ -37,60 +35,25 @@ resource "null_resource" "wait_for_k8s_api" {
   }
 }
 
-# Deploy applications
-module "app1" {
-  source = "./modules/apps"
+# Deploy applications using kubectl
+resource "null_resource" "deploy_apps" {
   depends_on = [null_resource.build_webapp_image, null_resource.wait_for_k8s_api]
   
-  app_name  = "app1"
-  image     = "webapp:latest"
-  namespace = "default"
-  replicas  = 2
-}
-
-module "app2" {
-  source = "./modules/apps"
-  depends_on = [null_resource.build_webapp_image, null_resource.wait_for_k8s_api]
-  
-  app_name  = "app2"
-  image     = "webapp:latest"
-  namespace = "default"
-  replicas  = 2
-}
-
-module "app3" {
-  source = "./modules/apps"
-  depends_on = [null_resource.build_webapp_image, null_resource.wait_for_k8s_api]
-  
-  app_name  = "app3"
-  image     = "webapp:latest"
-  namespace = "default"
-  replicas  = 2
-}
-
-# Setup ingress
-module "ingress" {
-  source = "./modules/ingress"
-  depends_on = [module.app1, module.app2, module.app3]
-  
-  namespace = "default"
-  host     = "localhost"
-  
-  apps = {
-    app1 = {
-      service_name = module.app1.service_name
-      app_name     = module.app1.app_name
-      namespace    = module.app1.namespace
-    }
-    app2 = {
-      service_name = module.app2.service_name
-      app_name     = module.app2.app_name
-      namespace    = module.app2.namespace
-    }
-    app3 = {
-      service_name = module.app3.service_name
-      app_name     = module.app3.app_name
-      namespace    = module.app3.namespace
-    }
+  provisioner "local-exec" {
+    command = <<-EOF
+      # Deploy ingress controller first
+      kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml --context kind-${var.cluster_name}
+      
+      # Wait for ingress controller to be ready
+      kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s --context kind-${var.cluster_name}
+      
+      # Deploy applications
+      kubectl apply -f k8s-manifests.yaml --context kind-${var.cluster_name}
+      
+      # Wait for applications to be ready
+      kubectl wait --for=condition=ready pod -l app=app1 --timeout=120s --context kind-${var.cluster_name}
+      kubectl wait --for=condition=ready pod -l app=app2 --timeout=120s --context kind-${var.cluster_name}
+      kubectl wait --for=condition=ready pod -l app=app3 --timeout=120s --context kind-${var.cluster_name}
+    EOF
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,12 +5,16 @@ output "cluster_name" {
 
 output "app_urls" {
   description = "URLs for accessing the applications"
-  value       = module.ingress.app_urls
+  value = {
+    app1 = "http://localhost/app1"
+    app2 = "http://localhost/app2"
+    app3 = "http://localhost/app3"
+  }
 }
 
 output "ingress_host" {
   description = "Ingress host"
-  value       = module.ingress.ingress_host
+  value       = "localhost"
 }
 
 output "instructions" {
